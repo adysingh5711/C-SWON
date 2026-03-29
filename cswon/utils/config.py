37fc6c72d5a4a35c9f2fd51319d0346919957a10
Aditya Singh <singhaditya5711@gmail.com>
2026-03-30 02:02:05 +0530

    fix(config): change WandB defaults to empty string to prevent opentensor-dev API errors

--- cswon/utils/config.py
+++ cswon/utils/config.py
@@ -162,14 +162,14 @@ def add_miner_args(cls, parser):
     parser.add_argument(
         "--wandb.project_name",
         type=str,
-        default="template-miners",
+        default="",
         help="Wandb project to log to.",
     )
 
     parser.add_argument(
         "--wandb.entity",
         type=str,
-        default="opentensor-dev",
+        default="",
         help="Wandb entity to log to.",
     )
 
@@ -240,14 +240,14 @@ def add_validator_args(cls, parser):
         "--wandb.project_name",
         type=str,
         help="The name of the project where you are sending the new run.",
-        default="template-validators",
+        default="",
     )
 
     parser.add_argument(
         "--wandb.entity",
         type=str,
         help="The name of the project where you are sending the new run.",
-        default="opentensor-dev",
+        default="",
     )
 
 
